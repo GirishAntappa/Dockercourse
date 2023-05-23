d2b780290d531513e14971e88a71f799adadcd0b
Girish <girivani95@gmail.com>
2023-05-23 15:10:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I am running good
         </p>
         <a
           className="App-link"
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          girish
         </a>
       </header>
     </div>
